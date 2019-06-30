ef45292f8c3f1ab99278496e366c3435dd4b6844
Bruce Szalwinski <szalwinski@csdisco.com>
2019-06-30 15:36:51 -0500

    cleanup

--- migration_util.py
+++ migration_util.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 from googleapiclient.discovery import build
-#from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import requests
